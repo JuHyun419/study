c12a701040e55e7cdb6f14c6401f3ea6bb398033
JuHyun419 <zzang9ha@naver.com>
2021-03-28 13:42:36 +0900

    [Programmers] level2 구명보트 풀이

--- Programmers/src/main/java/level2/구명보트.java
+++ Programmers/src/main/java/level2/구명보트.java
@@ -2,24 +2,22 @@ package level2;
 
 import java.util.Arrays;
 
-// TODO: 20210314
 public class 구명보트 {
 
-    public static int solution(int[] people, int limit) {
+    public int solution(int[] people, int limit) {
         int answer = 0;
-        int i = 0;
+        int left = 0;
         Arrays.sort(people);
 
-        for (int j = people.length - 1; i <= j; j--) {
-            if (people[j] + people[i] <= limit) {
-                i++;
+        for (int i = people.length - 1; i >= 0; i--) {
+            if (left > i) break;
+
+            if (people[i] + people[left] <= limit) {
+                left += 1;
             }
-            answer++;
+            answer ++;
         }
         return answer;
     }
 
-    public static void main(String[] args) {
-
-    }
 }
