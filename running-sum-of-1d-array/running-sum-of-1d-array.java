1c2745500b60f1928a2b70529ec7a511112538bf
JuHyun419 <zzang9ha@naver.com>
2021-05-06 22:11:15 +0900

    Remove directory

--- running-sum-of-1d-array/running-sum-of-1d-array.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public static int[] runningSum(int[] nums) {
-        for (int i = 1; i < nums.length; i++) {
-            nums[i] = nums[i-1] + nums[i];
-        }
-        return nums;
-    }
-}
