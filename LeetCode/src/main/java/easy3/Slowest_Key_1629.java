4556c4d0eada8bc9954e5299e682a7bf5f7a3790
JuHyun419 <zzang9ha@naver.com>
2021-05-24 20:45:11 +0900

    [LeetCode] easy 1629: Slowest Key

---
+++ LeetCode/src/main/java/easy3/Slowest_Key_1629.java
@@ -0,0 +1,29 @@
+package easy3;
+
+// TODO:
+public class Slowest_Key_1629 {
+
+    public char slowestKey(final int[] releaseTimes, final String keysPressed) {
+        final int length = releaseTimes.length;
+        int max = releaseTimes[0];
+        char ch = keysPressed.charAt(0);
+
+        for (int i = 1; i < length; i++) {
+            final int duration = releaseTimes[i] - releaseTimes[i - 1];
+            if (duration > max) {
+                max = duration;
+                ch = keysPressed.charAt(i);
+            } else if (duration == max) {
+                final int maxChar = ch - 'a';
+                final int maxDuration = keysPressed.charAt(i) - 'a';
+                if (maxDuration > maxChar) {
+                    ch = keysPressed.charAt(i);
+                }
+            }
+        }
+
+        return ch;
+    }
+
+
+}
