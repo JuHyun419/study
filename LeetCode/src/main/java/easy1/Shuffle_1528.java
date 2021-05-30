017a320629729de1804cf9ca3c708d05a7f323f5
JuHyun419 <zzang9ha@naver.com>
2021-05-30 13:29:01 +0900

    [LeetCode] easy 1528: Shuffle Remove TODO

--- LeetCode/src/main/java/easy1/Shuffle_1528.java
+++ LeetCode/src/main/java/easy1/Shuffle_1528.java
@@ -1,22 +1,20 @@
 package easy1;
 
-// TODO: 20210307 일요일(X)
 public class Shuffle_1528 {
 
     // 다른 풀이 - O(N)
-    public static String restoreString2(String s, int[] indices) {
-        char[] arr = new char[indices.length];
+    public static String restoreString2(final String s, final int[] indices) {
+        final char[] arr = new char[indices.length];
         for (int i = 0; i < indices.length; i++) {
             arr[indices[i]] = s.charAt(i);
         }
-
         return String.valueOf(arr);
     }
 
     // 내 풀이ㅠㅠ - O(N^2)
-    public static String restoreString(String s, int[] indices) {
-        int[] index = new int[indices.length];
-        StringBuilder sb = new StringBuilder();
+    public static String restoreString(final String s, final int[] indices) {
+        final int[] index = new int[indices.length];
+        final StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < indices.length; i++) {
             for (int j = 0; j < indices.length; j++) {
@@ -26,17 +24,15 @@ public class Shuffle_1528 {
                 }
             }
         }
-
-        for (int i : index) {
+        for (final int i : index) {
             sb.append(s.charAt(i));
         }
-
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        String s = "codeleet";
-        int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+    public static void main(final String[] args) {
+        final String s = "codeleet";
+        final int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
 
         System.out.println(restoreString(s, indices));
 
