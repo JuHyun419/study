d8f7c5c395f158ef7a94d50d0f8f8dfe17242b6d
JuHyun419 <zzang9ha@naver.com>
2021-05-31 10:24:21 +0900

    [LeetCode] easy 1624: Largest Substring Between Two Equal Characters Remove TODO

--- LeetCode/src/main/java/easy3/Largest_Substring_Between_Two_Equal_Characters_1624.java
+++ LeetCode/src/main/java/easy3/Largest_Substring_Between_Two_Equal_Characters_1624.java
@@ -1,37 +1,13 @@
 package easy3;
 
-// TODO:
 public class Largest_Substring_Between_Two_Equal_Characters_1624 {
 
-    // O(n^2)
     public static int maxLengthBetweenEqualCharacters(final String s) {
         int longest = -1;
-
         for (int i = 0; i < s.length(); i++) {
-            final char leftChar = s.charAt(i);
-
-            for (int j = i + 1; j < s.length(); j++) {
-                final char rightChar = s.charAt(j);
-                if (leftChar == rightChar) {
-                    final int length = (j - i) - 1;
-                    longest = Math.max(longest, length);
-                }
-            }
-        }
-        return longest;
-    }
-
-    // O(n)
-    public static int maxLengthBetweenEqualCharacters2(final String s) {
-        int longest = -1;
-
-        for (int i = 0; i < s.length(); i++) {
-            final char ch = s.charAt(i);
-            final int firstIndex = s.indexOf(ch);
-            final int lastIndex = s.lastIndexOf(ch);
-
-            if (lastIndex - firstIndex > 0) {
-                longest = Math.max(longest, (lastIndex - firstIndex) - 1);
+            final int lastIndex = s.lastIndexOf(s.charAt(i));
+            if (lastIndex != i && lastIndex != -1) {
+                longest = Math.max(longest, lastIndex - i - 1);
             }
         }
         return longest;
