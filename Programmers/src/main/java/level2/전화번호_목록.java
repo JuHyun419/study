c593054ebae40e41a580fcb2f49c452270329a57
JuHyun419 <zzang9ha@naver.com>
2021-06-02 09:51:20 +0900

    [Programmers] level2: 전화번호 목록 Remove TODO

--- Programmers/src/main/java/level2/전화번호_목록.java
+++ Programmers/src/main/java/level2/전화번호_목록.java
@@ -2,22 +2,20 @@ package level2;
 
 import java.util.Arrays;
 
-// TODO: 20210414
 public class 전화번호_목록 {
 
-    public boolean solution(String[] phoneBook) {
+    public boolean solution(final String[] phoneBook) {
         Arrays.sort(phoneBook);
 
         for (int i = 0; i < phoneBook.length - 1; i++) {
             if (phoneBook[i + 1].startsWith(phoneBook[i]))
                 return false;
         }
-
         return true;
     }
 
-    public static void main(String[] args) {
-        String[] str = {"119", "11964512", "11954512"};
+    public static void main(final String[] args) {
+        final String[] str = {"119", "11964512", "11954512"};
         Arrays.sort(str);
         System.out.println(Arrays.toString(str));
     }
