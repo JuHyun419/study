b67903eb12430225f81b86b89175a3681ed05f82
JuHyun419 <zzang9ha@naver.com>
2021-06-03 15:05:25 +0900

    Add Computer

---
+++ pair-programming-baseball/src/main/java/Computer.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class Computer {
+
+    private final List<Integer> numbers;
+
+    public Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+}
