feadaa4145e82c26cc40b2ea0b14f5787f144611
JuHyun419 <zzang9ha@naver.com>
2021-06-03 15:04:56 +0900

    Add Main class

---
+++ pair-programming-baseball/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //init
+        Game game = new Game();
+        Computer computer = new Computer(game.generateRandomNumber());
+
+        //for
+        //game.play(input, computer.getNumbers());
+//        game.countMatchNumbers(computer.getNumbers(), player.getNumbers());
+    }
+
+}
