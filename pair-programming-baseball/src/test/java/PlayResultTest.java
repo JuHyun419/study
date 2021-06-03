dd99192e26f0ca0f616c134411e6be92d88a8027
JuHyun419 <zzang9ha@naver.com>
2021-06-03 15:11:25 +0900

    test: 스트라이크, 볼 테스트 코드

---
+++ pair-programming-baseball/src/test/java/PlayResultTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayResultTest {
+
+    @DisplayName("스트라이크 테스트")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    public void test1(List<Integer> playerLists, List<Integer> computerLists, int index) {
+        PlayResult playResult = new PlayResult();
+        int strikeCount = playResult.countStrike(playerLists, computerLists);
+
+        int[] result = {1, 0};
+        assertEquals(strikeCount, result[index]);
+    }
+
+    @DisplayName("볼 테스트")
+    @ParameterizedTest
+    @MethodSource("generateData")
+    void test_ball(List<Integer> playerLists, List<Integer> computerLists, int index) {
+        PlayResult playResult = new PlayResult();
+        int ballCount = playResult.countBall(playerLists, computerLists);
+
+        int[] result = {2, 0};
+        assertEquals(ballCount, result[index]);
+    }
+
+    static Stream<Arguments> generateData() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2), 0),
+                Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), 1)
+        );
+    }
+}
