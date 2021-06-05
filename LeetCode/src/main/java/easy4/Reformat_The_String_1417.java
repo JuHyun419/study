2c25681ef92f30474378d5d5936ce290a5f4f11d
JuHyun419 <zzang9ha@naver.com>
2021-06-05 11:24:35 +0900

    [LeetCode] easy 1417: Reformat The String

---
+++ LeetCode/src/main/java/easy4/Reformat_The_String_1417.java
@@ -0,0 +1,70 @@
+package easy4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+// TODO: 최적화 & 풀이
+public class Reformat_The_String_1417 {
+    public static String reformat(String s) {
+        StringBuilder sb = new StringBuilder();
+        if (s.length() == 1) return s;
+        if (isAlpha(s) || isNumeric(s)) return "";
+        List<Integer> numbers = new ArrayList<>();
+        List<Character> alphas = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                alphas.add(s.charAt(i));
+            } else {
+                numbers.add(s.charAt(i) - '0');
+            }
+        }
+
+        if (numbers.size() > alphas.size()) {
+            for (int i = 0; i < alphas.size(); i++) {
+                sb.append(numbers.get(i));
+                sb.append(alphas.get(i));
+            }
+            sb.append(numbers.get(numbers.size() - 1));
+        } else if (numbers.size() < alphas.size()){
+            for (int i = 0; i < numbers.size(); i++) {
+                sb.append(alphas.get(i));
+                sb.append(numbers.get(i));
+            }
+            sb.append(alphas.get(alphas.size() - 1));
+        } else {
+            for (int i = 0; i < numbers.size(); i++) {
+                sb.append(alphas.get(i));
+                sb.append(numbers.get(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    private static boolean isNumeric(String s) {
+        final Pattern pattern = Pattern.compile("\\d+");
+        return pattern.matcher(s).matches();
+    }
+
+    private static boolean isNumeric2(String s) {
+        try {
+            Long number = Long.parseLong(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isAlpha(String s) {
+        final String regex = "^[a-zA-Z]*$";
+        return s.matches(regex);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "covid2019";
+        String str2 = "ab123";
+        System.out.println(reformat(str1));
+        System.out.println(reformat(str2));
+    }
+}
