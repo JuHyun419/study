5c9da1d86f5bf9d44009f36206b4c18c2d950cea
JuHyun419 <zzang9ha@naver.com>
2021-06-07 10:33:04 +0900

    [Programmers] 고득점 Kit: (Heap) 이중우선순위큐

---
+++ Programmers/src/main/java/kit/heap/이중우선순위큐.java
@@ -0,0 +1,45 @@
+package kit.heap;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class 이중우선순위큐 {
+    public static int[] solution(String[] operations) {
+        int[] answer = {};
+        Queue<Integer> minQueue = new PriorityQueue<>();
+        Queue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int i = 0; i < operations.length; i++) {
+            String operation = operations[i].split(" ")[0];
+            int number = Integer.parseInt(operations[i].split(" ")[1]);
+
+            if (operation.equals("I")) {
+                minQueue.add(number);
+                maxQueue.add(number);
+            } else if (operation.equals("D")) {
+                if (minQueue.isEmpty() || maxQueue.isEmpty()) continue;
+                if (number == 1) { // 최댓값 삭제
+                    int max = maxQueue.peek();
+                    maxQueue.poll();
+                    minQueue.remove(max);
+                } else if (number == -1) { // 최솟값 삭제
+                    int min = minQueue.peek();
+                    minQueue.poll();
+                    maxQueue.remove(min);
+                }
+            }
+        }
+
+        return minQueue.isEmpty()
+                ? new int[]{0, 0}
+                : new int[]{maxQueue.peek(), minQueue.peek()};
+    }
+
+    public static void main(String[] args) {
+        String[] operations = {"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"};
+        System.out.println(Arrays.toString(solution(operations)));
+
+    }
+}
