4d18505ab964a2187ad59599573554e0ea75963b
JuHyun419 <zzang9ha@naver.com>
2021-06-10 19:24:02 +0900

    initial commit

---
+++ codeweb-springboot/src/main/java/org/zerock/ex1/CodewebSpringbootApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewebSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodewebSpringbootApplication.class, args);
+    }
+
+}
