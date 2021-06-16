e1523538197b9eb5df25f53832f05f4d261aa104
JuHyun419 <zzang9ha@naver.com>
2021-06-16 09:42:31 +0900

    [LeetCode] easy 1089: Duplicate Zeros

---
+++ Algorithm/LeetCode/src/main/java/easy4/Duplicate_Zeros_1089.java
@@ -0,0 +1,65 @@
+package easy4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// TODO:
+public class Duplicate_Zeros_1089 {
+    public void duplicateZeros(int[] arr) {
+        if (isNotContainsZero(arr)) {
+            return;
+        }
+
+        List<Integer> list = toList(arr);
+
+        for (int i = 0; i < list.size(); i++) {
+            int number = list.get(i);
+            if (number == 0) {
+                list.add((i + 1), number);
+                list.remove(list.size() - 1);
+                i++;
+            }
+        }
+
+        arr = toArray(list);
+    }
+
+    private boolean isNotContainsZero(int[] array) {
+        for (int num : array) {
+            if (num == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private List<Integer> toList(int[] arr) {
+        return Arrays.stream(arr).boxed().collect(Collectors.toList());
+    }
+
+    private int[] toArray(List<Integer> list) {
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    // TODO: 풀이 확인(index를 이용한 풀이)
+    public void duplicateZeros2(int[] arr) {
+        int[] newArr = new int[arr.length];
+        int p2 = 0;
+        for (int i = 0; i < arr.length && p2 < arr.length; i++) {
+            if (arr[i] == 0) {
+                p2++;
+            } else {
+                newArr[p2] = arr[i];
+            }
+            p2++;
+        }
+        System.arraycopy(newArr, 0, arr, 0, arr.length);
+    }
+
+    public static void main(String[] args) {
+        Duplicate_Zeros_1089 d = new Duplicate_Zeros_1089();
+        int[] arr = {1, 0, 2, 3, 0, 4, 5, 0};
+        d.duplicateZeros2(arr);
+    }
+}
