1d16adbaae9473ab24c351e7f06fc9dd63b3416f
JuHyun419 <zzang9ha@naver.com>
2021-06-22 00:29:58 +0900

    [Programmers] SQL 고득점 kit: (String, Date)

---
+++ Algorithm/Programmers/src/main/java/sqlkit/stringdate/루시와_엘라_찾기.sql
@@ -0,0 +1,5 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID;
---
+++ Algorithm/Programmers/src/main/java/sqlkit/stringdate/중성화_여부_파악하기.sql
@@ -0,0 +1,16 @@
+-- TODO:
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME,
+       CASE
+           WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O'
+           WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O'
+           ELSE 'X'
+        END AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+
+SELECT ANIMAL_ID, NAME,
+IF(SEX_UPON_INTAKE LIKE '%Neutered%' OR SEX_UPON_INTAKE LIKE '%Spayed%','O','X') AS '중성화'
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
