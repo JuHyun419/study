d20f7a6e43c549dcf600f2a7d898fced8e4185d1
JuHyun419 <zzang9ha@naver.com>
2021-06-24 13:49:04 +0900

    [LeetCode] easy 1380: Lucky Numbers in a Matrix

---
+++ Algorithm/LeetCode/src/main/java/easy5/Lucky_Numbers_in_a_Matrix_1380.java
@@ -0,0 +1,44 @@
+package easy5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO:
+public class Lucky_Numbers_in_a_Matrix_1380 {
+    public static List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            int minColumn = 0;
+
+            // find minimum element in row
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                    minColumn = j;
+                }
+            }
+
+            // find maximum element in column
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[j][minColumn] > max) {
+                    max = matrix[j][minColumn];
+                }
+            }
+
+            if (min == max) {
+                list.add(min);
+                break;
+            }
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{3, 8, 7}, {9, 16, 13}, {15, 16, 17}};
+
+        luckyNumbers(matrix);
+    }
+}
