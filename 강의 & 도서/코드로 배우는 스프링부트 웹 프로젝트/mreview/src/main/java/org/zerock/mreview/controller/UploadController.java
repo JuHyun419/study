952ec34b6783e9c0617629337d0bf2849653ebb9
JuHyun419 <zzang9ha@naver.com>
2021-06-28 16:08:58 +0900

    feat: 파일 업로드 처리 코드 추가

---
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/mreview/src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -0,0 +1,139 @@
+package org.zerock.mreview.controller;
+
+import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDto;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@Log4j2
+public class UploadController {
+
+    @Value("${org.zerock.upload.path}")
+    private String uploadPath;
+
+    @PostMapping("/uploadAjax")
+    public ResponseEntity<List<UploadResultDto>> uploadFile(MultipartFile[] uploadFiles) {
+        Assert.notNull(uploadFiles, "Files must not be null!");
+
+        List<UploadResultDto> resultDtoList = new ArrayList<>();
+
+        for (MultipartFile uploadFile : uploadFiles) {
+            // 이미지 파일만 업로드
+            if (!uploadFile.getContentType().startsWith("image")) {
+                log.warn("This file is not image type");
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+            final String originalName = uploadFile.getOriginalFilename();
+            Assert.notNull(originalName, "file is not exist");
+            final String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
+
+            log.info("fileName: " + fileName);
+            
+            // 날짜 폴더 생성
+            final String folderPath = makeFolder(); // /Users/juhyun/Desktop/study/files/2021/06/28
+            
+            // UUID
+            final String uuid = UUID.randomUUID().toString();
+
+            // 저장할 파일 이름 중간에 "_"를 이용해서 구분 => /Users/juhyun/Desktop/study/files/2021/06/28 + uuid_fileName
+            final String saveName = uploadPath + File.separator + folderPath + File.separator + uuid + "_" + fileName;
+            Path savePath = Paths.get(saveName);
+
+            try {
+                // 원본 파일 저장
+                uploadFile.transferTo(savePath);
+
+                // 썸네일 파일명(s_)
+                String thumbnail = uploadPath + File.separator + folderPath + File.separator + "s_" + uuid + "_" + fileName;
+                File thumbnailFile = new File(thumbnail);
+
+                // 썸네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile, 100, 100);
+
+                resultDtoList.add(new UploadResultDto(fileName, uuid, folderPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return new ResponseEntity<>(resultDtoList, HttpStatus.OK);
+    }
+
+    private String makeFolder() {
+        // 2021/06/28
+        final String now = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+        final String folderPath = now.replace("//", File.separator);
+        // uploadPathFolder path : /Users/juhyun/Desktop/study/files /2021/06/28
+        final File uploadPathFolder = new File(uploadPath, folderPath);
+
+        if (!uploadPathFolder.exists()) {
+            uploadPathFolder.mkdirs();
+        }
+        return folderPath;
+    }
+
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result;
+
+        try {
+            String srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            log.info("decode fileName: " + srcFileName);
+            File file = new File(uploadPath + File.separator + srcFileName);
+            log.info("file: " + file);
+
+            HttpHeaders header = new HttpHeaders();
+
+            // MIME 타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+
+            // 파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
+
+    @PostMapping("/remove")
+    public ResponseEntity<Boolean> remove(String fileName) {
+        String srcFileName;
+        try {
+            srcFileName = URLDecoder.decode(fileName, "UTF-8");
+            File file = new File(uploadPath + File.separator + srcFileName);
+            boolean result = file.delete();
+
+            File thumbnail = new File(file.getParent(), "s_" + file.getName());
+            result = thumbnail.delete();
+
+            return new ResponseEntity<>(result, HttpStatus.OK);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/mreview/src/main/java/org/zerock/mreview/controller/UploadTestController.java
@@ -0,0 +1,13 @@
+package org.zerock.mreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadTestController {
+
+    @GetMapping("/uploadEx")
+    public void uploadEx() {
+
+    }
+}
