85c1c02339359a8fd97084ba2ca140f6309e605c
JuHyun419 <zzang9ha@naver.com>
2021-06-30 22:50:15 +0900

    [LeetCode] easy 1: Two Sum

---
+++ Algorithm/LeetCode/src/main/java/easy5/Two_Sum_1.java
@@ -0,0 +1,20 @@
+package easy5;
+
+public class Two_Sum_1 {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (i == j) continue;
+
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+}
