bd1503af32674d0d484e35e4c922bd354c5d4be3
JuHyun419 <zzang9ha@naver.com>
2021-07-02 15:51:04 +0900

    initial commit - add Stream API Example

---
+++ java-example/src/main/java/streamapi/OnlineClass.java
@@ -0,0 +1,49 @@
+package streamapi;
+
+public class OnlineClass {
+
+    private Integer id;
+
+    private String title;
+
+    private boolean closed;
+
+    public OnlineClass(Integer id, String title, boolean closed) {
+        this.id = id;
+        this.title = title;
+        this.closed = closed;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isClosed() {
+        return closed;
+    }
+
+    public void setClosed(boolean closed) {
+        this.closed = closed;
+    }
+
+    @Override
+    public String toString() {
+        return "OnlineClass{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", closed=" + closed +
+                '}';
+    }
+}
