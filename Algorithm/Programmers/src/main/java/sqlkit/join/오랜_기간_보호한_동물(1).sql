e973076eefe4e5abed83a4bc7a4391e5965f1948
JuHyun419 <zzang9ha@naver.com>
2021-07-04 17:40:16 +0900

    [Programmers] SQL 고득점 kit: (JOIN) Remove TODO

--- Algorithm/Programmers/src/main/java/sqlkit/join/오랜_기간_보호한_동물(1).sql
+++ Algorithm/Programmers/src/main/java/sqlkit/join/오랜_기간_보호한_동물(1).sql
@@ -1,4 +1,3 @@
--- TODO:
 -- 코드를 입력하세요
 SELECT a.NAME, a.DATETIME
 FROM ANIMAL_INS a
@@ -6,9 +5,3 @@ LEFT JOIN ANIMAL_OUTS b
 ON a.ANIMAL_ID = b.ANIMAL_ID
 ORDER BY a.DATETIME
 LIMIT 3;
-
-SELECT b.bno, b.title, b.writer_email, r.rno, r.text
-FROM Board b
-LEFT OUTER JOIN Reply r
-ON b.bno = r.board_bno
-WHERE b.bno = 100;
