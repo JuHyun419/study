39615aaf25dafe3033beb553d283ffe982604175
JuHyun419 <zzang9ha@naver.com>
2021-07-06 10:02:13 +0900

    Change Node -> ListNode

--- Algorithm/LeetCode/src/main/java/easy5/Delete_Node_in_a_Linked_List_237.java
+++ Algorithm/LeetCode/src/main/java/easy5/Delete_Node_in_a_Linked_List_237.java
@@ -1,14 +1,5 @@
 package easy5;
 
-class Node3 {
-    int val;
-    Node3 next;
-
-    public Node3(int val) {
-        this.val = val;
-    }
-}
-
 public class Delete_Node_in_a_Linked_List_237 {
     public void deleteNode(ListNode node) {
         if (node == null) return;
