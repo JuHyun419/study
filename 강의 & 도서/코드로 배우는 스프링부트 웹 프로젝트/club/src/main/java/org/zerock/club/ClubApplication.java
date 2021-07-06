f39878217e930179191ec8e878db362bd3b3395b
JuHyun419 <zzang9ha@naver.com>
2021-07-06 19:21:57 +0900

    Add Enable Jpa Auditing

--- 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/main/java/org/zerock/club/ClubApplication.java
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/main/java/org/zerock/club/ClubApplication.java
@@ -2,8 +2,10 @@ package org.zerock.club;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ClubApplication {
 
     public static void main(String[] args) {
