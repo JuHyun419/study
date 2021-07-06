8313da426d92301be7cef74f9fa9a0424c00f7c0
JuHyun419 <zzang9ha@naver.com>
2021-07-06 13:37:26 +0900

    initial commit

---
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/test/java/org/zerock/club/ClubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.club;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
