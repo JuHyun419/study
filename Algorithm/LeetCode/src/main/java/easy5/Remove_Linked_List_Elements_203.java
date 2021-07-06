fd37ae048bd93ff2ff672f0b12cb574a7572709f
JuHyun419 <zzang9ha@naver.com>
2021-07-06 10:02:13 +0900

    Change Node -> ListNode

--- Algorithm/LeetCode/src/main/java/easy5/Remove_Linked_List_Elements_203.java
+++ Algorithm/LeetCode/src/main/java/easy5/Remove_Linked_List_Elements_203.java
@@ -1,29 +1,13 @@
 package easy5;
 
 // TODO:
-class Node {
-    int val;
-    Node next;
-
-    public Node() {
-    }
-
-    public Node(int val) {
-        this.val = val;
-    }
-
-    public Node(int val, Node next) {
-        this.val = val;
-        this.next = next;
-    }
-}
 
 public class Remove_Linked_List_Elements_203 {
-    public Node removeElements(Node head, int val) {
+    public ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
 
-        Node current = head.next;
-        Node prev = head;
+        ListNode current = head.next;
+        ListNode prev = head;
 
         while (current != null) {
             if (current.val == val) {
@@ -40,14 +24,14 @@ public class Remove_Linked_List_Elements_203 {
         return head;
     }
 
-    public Node removeElements2(Node head, int val) {
+    public ListNode removeElements2(ListNode head, int val) {
         if (head == null) return null;
 
         head.next = removeElements2(head.next, val);
         return head.val == val ? head.next : head;
     }
 
-    public Node removeElements3(Node head, int val) {
+    public ListNode removeElements3(ListNode head, int val) {
         if (head == null) return null;
 
         if (head.val == val) head = removeElements3(head.next, val);
--- Algorithm/LeetCode/src/main/java/easy5/Reverse_Linked_List_206.java
+++ Algorithm/LeetCode/src/main/java/easy5/Reverse_Linked_List_206.java
@@ -1,33 +1,16 @@
 package easy5;
 
 // TODO:
-class Node2 {
-    int val;
-    Node2 next;
-
-    public Node2() {
-    }
-
-    public Node2(int val) {
-        this.val = val;
-    }
-
-    public Node2(int val, Node2 next) {
-        this.val = val;
-        this.next = next;
-    }
-}
-
 public class Reverse_Linked_List_206 {
 
-    public Node2 reverseList(Node2 head) {
+    public ListNode reverseList(ListNode head) {
         if (head == null) return null;
 
-        Node2 prev = null;
-        Node2 current = head;
+        ListNode prev = null;
+        ListNode current = head;
 
         while (current != null) {
-            Node2 temp = current.next;
+            ListNode temp = current.next;
             current.next = prev;
             prev = current;
             current = temp;
