dbb036c7dbc2891215a6ca9e6d3a3024d9d14605
JuHyun419 <zzang9ha@naver.com>
2021-07-07 14:55:21 +0900

    feat: 스프링 시큐리티 View & Controller 에서 Principal 확인 코드 추가

--- 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/main/java/org/zerock/club/controller/SampleController.java
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/main/java/org/zerock/club/controller/SampleController.java
@@ -1,9 +1,11 @@
 package org.zerock.club.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.club.security.ClubAuthMemberDto;
 
 @Controller
 @Log4j2
@@ -18,8 +20,9 @@ public class SampleController {
 
     // 로그인한 사용자만 접근 가능
     @GetMapping("/member")
-    public void member() {
+    public void member(@AuthenticationPrincipal ClubAuthMemberDto clubAuthMemberDto) {
         log.info("member......");
+        log.info("Auth Member: " + clubAuthMemberDto);
     }
 
     // 관리자(admin) 권한을 가진 사용자만 접근 가능
