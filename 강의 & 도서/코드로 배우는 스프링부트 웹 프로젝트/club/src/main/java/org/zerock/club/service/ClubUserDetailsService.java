3e141de995696cb5c5f4e89c6bf722d79ce7e567
JuHyun419 <zzang9ha@naver.com>
2021-07-07 14:47:19 +0900

    feat: 스프링 시큐리티 코드 추가 및 설정 수정

---
+++ 강의 & 도서/코드로 배우는 스프링부트 웹 프로젝트/club/src/main/java/org/zerock/club/service/ClubUserDetailsService.java
@@ -0,0 +1,56 @@
+package org.zerock.club.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.zerock.club.entity.ClubMember;
+import org.zerock.club.entity.ClubMemberRole;
+import org.zerock.club.repository.ClubMemberRepository;
+import org.zerock.club.security.ClubAuthMemberDto;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class ClubUserDetailsService implements UserDetailsService {
+
+    private final ClubMemberRepository clubMemberRepository;
+
+    // username -> 실제 로그인하는 email
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("ClubUserDetailsService username: " + username);
+
+        ClubMember clubMember = clubMemberRepository.findByEmail(username, false)
+                .orElseThrow(() -> new UsernameNotFoundException("Check Email or Social"));
+
+        log.info("clubMember: " + clubMember);
+
+        ClubAuthMemberDto clubAuthMemberDto = new ClubAuthMemberDto(
+                clubMember.getEmail(),
+                clubMember.getPassword(),
+                clubMember.isFromSocial(),
+                getAuthority(clubMember.getRoleSet())
+                //clubMember.getRoleSet().stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role.name())).collect(Collectors.toList())
+        );
+
+        clubAuthMemberDto.setName(clubMember.getName());
+        clubAuthMemberDto.setFromSocial(clubMember.isFromSocial());
+
+        return clubAuthMemberDto;
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthority(Set<ClubMemberRole> roleSet) {
+        return roleSet.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toList());
+    }
+}
