90ed318234229244971e550dc2376b653769f973
JuHyun419 <zzang9ha@naver.com>
2021-07-16 14:42:20 +0900

    [SWExpert] D3 7510: 상원이의 연속 합

---
+++ Algorithm/SWExpert/src/main/java/d3/상원이의_연속_합_7510.java
@@ -0,0 +1,32 @@
+package d3;
+
+import java.util.Scanner;
+
+// TODO:
+public class 상원이의_연속_합_7510 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = scanner.nextInt();
+            int answer = 1;
+
+            for (int i = 1; i <= N / 2; i++) {
+                int sum = 0;
+                for (int j = i; ; j++) {
+                    sum += j;
+                    if (sum == N) {
+                        answer ++;
+                    }
+                    if (sum > N) {
+                        break;
+                    }
+                }
+            }
+            System.out.println("#" + tc + " " + answer);
+        }
+        scanner.close();
+    }
+}
