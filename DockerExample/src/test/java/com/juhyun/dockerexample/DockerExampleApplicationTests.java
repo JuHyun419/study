4e6d6707773a98d61ed973bb6172ee5edf5913d7
JuHyun419 <zzang9ha@naver.com>
2021-07-19 19:55:51 +0900

    스프링부트 + 도커 + AWS 배포 예제 추가

---
+++ DockerExample/src/test/java/com/juhyun/dockerexample/DockerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhyun.dockerexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
