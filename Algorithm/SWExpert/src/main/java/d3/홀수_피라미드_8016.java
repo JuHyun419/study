eac94c080359c592bd9c4b32d532ab148b67854a
JuHyun419 <zzang9ha@naver.com>
2021-07-19 13:30:24 +0900

    [SWExpert] D3 8016: 홀수 피라미드

---
+++ Algorithm/SWExpert/src/main/java/d3/홀수_피라미드_8016.java
@@ -0,0 +1,18 @@
+package d3;
+
+import java.util.Scanner;
+
+// TODO: 규칙 찾기
+public class 홀수_피라미드_8016 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int T = scanner.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            final long N = scanner.nextLong();
+            long left = (N - 1) * (N - 1) * 2 + 1;
+            long right = N * N * 2 - 1; // 다음 층 left - 2
+            System.out.println("#" + tc + " " + left + " " + right);
+        }
+        scanner.close();
+    }
+}
