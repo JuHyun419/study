f774de3ca1df32fcfa7602b7417324f257189a7f
JuHyun419 <zzang9ha@naver.com>
2021-07-22 10:36:58 +0900

    [Programmers] Dev-Matching: 행렬 테두리 회전하기 주석 제거

--- Algorithm/Programmers/src/main/java/devmatching/행렬_테두리_회전하기.java
+++ Algorithm/Programmers/src/main/java/devmatching/행렬_테두리_회전하기.java
@@ -9,11 +9,10 @@ public class 행렬_테두리_회전하기 {
         int[][] matrix = getMatrix(rows, columns);
 
         for (int index = 0; index < queries.length; index++) {
-            // (2,2) ~ (5,4)
-            final int x1 = queries[index][0] - 1; // 1
-            final int y1 = queries[index][1] - 1; // 1
-            final int x2 = queries[index][2] - 1; // 4
-            final int y2 = queries[index][3] - 1; // 3
+            final int x1 = queries[index][0] - 1;
+            final int y1 = queries[index][1] - 1;
+            final int x2 = queries[index][2] - 1;
+            final int y2 = queries[index][3] - 1;
             int temp = matrix[x1][y1]; // 가장 첫번째 위치하는 값 -> 한 칸 오른쪽으로 회전
             int min = temp;
 
