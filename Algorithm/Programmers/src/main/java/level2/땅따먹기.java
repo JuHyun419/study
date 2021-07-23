bc8f30a4d11a7c474475c97693c20c1ca2f02600
JuHyun419 <zzang9ha@naver.com>
2021-07-23 10:29:41 +0900

    [Programmers] level2: 땅따먹기 Remove TODO

--- Algorithm/Programmers/src/main/java/level2/땅따먹기.java
+++ Algorithm/Programmers/src/main/java/level2/땅따먹기.java
@@ -1,11 +1,8 @@
 package level2;
 
-
-// TODO:
 public class 땅따먹기 {
 
     public static int solution(int[][] land) {
-
         // 한 행씩 내려가면서 이전 행과 더해서 최댓값 구하기(같은 열은 연속해서 밟을 수 없음)
         for (int i = 1; i < land.length; i++) {
             land[i][0] += Math.max(land[i - 1][1], Math.max(land[i - 1][2], land[i - 1][3]));
@@ -13,7 +10,6 @@ public class 땅따먹기 {
             land[i][2] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][3]));
             land[i][3] += Math.max(land[i - 1][0], Math.max(land[i - 1][1], land[i - 1][2]));
         }
-
         int size = land.length - 1;
 
         // 마지막 행에서 최댓값
