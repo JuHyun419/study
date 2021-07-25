1a4ddeade1fd2ea44a89313d7e4983a78e0c2689
JuHyun419 <zzang9ha@naver.com>
2021-07-25 16:49:46 +0900

    [Programmers] level2: 기능개발 코드 수정

--- Algorithm/Programmers/src/main/java/level2/기능개발.java
+++ Algorithm/Programmers/src/main/java/level2/기능개발.java
@@ -5,35 +5,42 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
 
+// TODO: 8월 중순 이후에 다시 풀어볼 것
 public class 기능개발 {
     private static final int PROCESS = 100;
 
     public static int[] solution(final int[] progresses, final int[] speeds) {
-        final List<Integer> list = new ArrayList<>();
-        final int length = progresses.length;
-        final Stack<Integer> stack = new Stack<>();
+        List<Integer> features = new ArrayList<>();
+        final Stack<Integer> workDays = getWorkDays(progresses, speeds);
 
-        for (int i = length - 1; i >= 0; i--) {
-            final int days = (int) Math.ceil((double)(PROCESS - progresses[i]) / speeds[i]);
-            stack.push(days);
-        }
-
-        while (!stack.isEmpty()) {
+        while (!workDays.isEmpty()) {
             int deployCount = 1;
-            final int top = stack.pop();
-            while (!stack.isEmpty() && stack.peek() <= top) {
+            final int top = workDays.pop();
+            while (!workDays.isEmpty() && workDays.peek() <= top) {
                 deployCount ++;
-                stack.pop();
+                workDays.pop();
             }
-            list.add(deployCount);
+            features.add(deployCount);
         }
+        return listToArray(features);
+    }
+
+    private static Stack<Integer> getWorkDays(int[] progresses, int[] speeds) {
+        Stack<Integer> workDays = new Stack<>();
+        for (int i = progresses.length - 1; i >= 0; i--) {
+            final int days = (int) Math.ceil((double)(PROCESS - progresses[i]) / speeds[i]);
+            workDays.push(days);
+        }
+        return workDays;
+    }
+
+    private static int[] listToArray(final List<Integer> list) {
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 
     public static void main(final String[] args) {
         final int[] progresses = {95, 90, 99, 99, 80, 99};
         final int[] speeds = {1, 1, 1, 1, 1, 1};
-
         System.out.println(Arrays.toString(solution(progresses, speeds)));
     }
 }
