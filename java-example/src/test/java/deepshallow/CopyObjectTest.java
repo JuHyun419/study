bcb0636f108b3951db771880e817b4cb0cd282c9
JuHyun419 <zzang9ha@naver.com>
2021-07-30 16:52:41 +0900

    java-example: Java 깊은복사, 얕은복사 예제 코드 추가

---
+++ java-example/src/test/java/deepshallow/CopyObjectTest.java
@@ -0,0 +1,25 @@
+package deepshallow;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CopyObjectTest {
+
+    @Test
+    void shallowCopy() {
+        CopyObject original = new CopyObject("JuHyun", 20);
+        CopyObject copyConstructor = new CopyObject(original);
+        CopyObject copyFactory = CopyObject.copy(original);
+
+        copyConstructor.setName("JuBal");
+        copyFactory.setName("BalJu");
+
+        System.out.println(original);
+        System.out.println(copyConstructor);
+        System.out.println(copyFactory);
+        System.out.println(original.getName());
+        System.out.println(copyConstructor.getName());
+        System.out.println(copyFactory.getName());
+    }
+}
