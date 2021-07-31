2b2bb4867cbffad31047bea7b33bcccb60a65547
JuHyun419 <zzang9ha@naver.com>
2021-07-31 13:03:07 +0900

    springboot-jwt: Add Entity & DTO

---
+++ springboot-jwt-tutorial/src/main/java/com/juhyun/springbootjwttutorial/dto/LoginDto.java
@@ -0,0 +1,26 @@
+package com.juhyun.springbootjwttutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginDto {
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+}
---
+++ springboot-jwt-tutorial/src/main/java/com/juhyun/springbootjwttutorial/dto/TokenDto.java
@@ -0,0 +1,17 @@
+package com.juhyun.springbootjwttutorial.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+
+    private String token;
+}
