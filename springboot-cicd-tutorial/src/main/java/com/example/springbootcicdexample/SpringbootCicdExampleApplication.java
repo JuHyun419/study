edfadc80ac153a1454e776b4d99cd169f8a623e8
JuHyun419 <zzang9ha@naver.com>
2021-08-02 12:27:33 +0900

    springboot-cicd-tutorial: initial commit

---
+++ springboot-cicd-tutorial/src/main/java/com/example/springbootcicdexample/SpringbootCicdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCicdExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCicdExampleApplication.class, args);
+    }
+
+}
---
+++ springboot-cicd-tutorial/src/test/java/com/example/springbootcicdexample/SpringbootCicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootcicdexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCicdExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
