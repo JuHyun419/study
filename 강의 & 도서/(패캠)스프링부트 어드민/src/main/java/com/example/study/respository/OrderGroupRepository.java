08307807291e8796c3617bbb00497acef64fa882
JuHyun419 <zzang9ha@naver.com>
2021-08-04 17:25:46 +0900

    Remove..

--- 강의 & 도서/(패캠)스프링부트 어드민/src/main/java/com/example/study/respository/OrderGroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.study.respository;
-
-import com.example.study.model.entity.OrderGroup;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderGroupRepository extends JpaRepository<OrderGroup, Long> {
-
-}
