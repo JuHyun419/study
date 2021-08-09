0863938bd7561a61d9cff15ed38281e9bb349dd4
JuHyun419 <zzang9ha@naver.com>
2021-08-09 14:23:34 +0900

    [LeetCode] String 290: Word Pattern

---
+++ Algorithm/LeetCode/src/main/java/string/Word_Pattern_290.java
@@ -0,0 +1,32 @@
+package string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// TODO:
+public class Word_Pattern_290 {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> charMap = new HashMap<>();
+        Map<String, Character> strMap = new HashMap<>();
+        String[] words = s.split(" ");
+        char[] charArray = pattern.toCharArray();
+
+        if (pattern.length() != words.length) {
+            return false;
+        }
+
+        for (int i = 0; i < charArray.length; i++) {
+            // pattern: "abba", s: "dog cat cat fish"
+            if (charMap.containsKey(charArray[i]) && !charMap.get(charArray[i]).equals(words[i])) {
+                return false;
+            }
+            // pattern: "abba", s: "dog dog dog dog"
+            if (strMap.containsKey(words[i]) && !strMap.get(words[i]).equals(charArray[i])) {
+                return false;
+            }
+            charMap.put(charArray[i], words[i]);
+            strMap.put(words[i], charArray[i]);
+        }
+        return true;
+    }
+}
