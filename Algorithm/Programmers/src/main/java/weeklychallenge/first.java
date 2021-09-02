81d5b4757dcba3d38c2efff55e1cba6695497d6f
JuHyun419 <zzang9ha@naver.com>
2021-09-02 10:45:44 +0900

    refactor: 소문자 -> 대문자 수정

--- Algorithm/Programmers/src/main/java/weeklychallenge/first.java
+++ Algorithm/Programmers/src/main/java/weeklychallenge/first.java
@@ -1,6 +1,6 @@
 package weeklychallenge;
 
-public class first {
+public class First {
     public long solution(int price, int money, int count) {
         long sum = 0;
         for (int i = 1; i <= count; i++) {
