8de672a0a104c9125428e286e341005600067a53
JuHyun419 <zzang9ha@naver.com>
2021-09-02 22:39:10 +0900

    refactor: Code Format

--- Kotlin-SpringBoot/src/main/kotlin/org/juhyun/kotlinspringboot/model/UserRequest.kt
+++ Kotlin-SpringBoot/src/main/kotlin/org/juhyun/kotlinspringboot/model/UserRequest.kt
@@ -1,19 +1,15 @@
 package org.juhyun.kotlinspringboot.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-// Json 네이밍 전략
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 // @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class) deprecated
-data class UserRequest (
-        var name:String?=null,
-        var age:Int?=null,
-        var email:String?=null,
-        var address:String?=null,
-
-        @JsonProperty("phone_number")
-        var phoneNumber:String?=null // json에서는 phone_number
+data class UserRequest(
+        var name: String? = null,
+        var age: Int? = null,
+        var email: String? = null,
+        var address: String? = null,
+        var phoneNumber: String? = null
 )
