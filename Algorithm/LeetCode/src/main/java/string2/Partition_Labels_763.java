4e73ef51166e1c3fb4ff57b41f833183d991601c
JuHyun419 <zzang9ha@naver.com>
2021-09-03 09:43:25 +0900

    refactor: 변수 선언 위치 수정 & 메소드 추가

--- Algorithm/LeetCode/src/main/java/string2/Partition_Labels_763.java
+++ Algorithm/LeetCode/src/main/java/string2/Partition_Labels_763.java
@@ -3,26 +3,30 @@ package string2;
 import java.util.ArrayList;
 import java.util.List;
 
-// TODO:
+// TODO: 9월말~ 풀어보기
 public class Partition_Labels_763 {
     public List<Integer> partitionLabels(String s) {
         int[] alphabet = new int[26];
-
-        for (int i = 0; i < s.length(); i++) {
-            alphabet[s.charAt(i) - 'a'] = i;
-        }
         int max = 0;
         int position = 0;
         List<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < s.length(); i++) {
+            alphabet[s.charAt(i) - 'a'] = i;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
             max = Math.max(max, alphabet[s.charAt(i) - 'a']);
             position++;
-            if (i == max) {
+            if (isLastPartitionLetters(max, i)) {
                 list.add(position);
                 position = 0;
             }
         }
         return list;
+    }
+
+    private boolean isLastPartitionLetters(int max, int i) {
+        return i == max;
     }
 }
