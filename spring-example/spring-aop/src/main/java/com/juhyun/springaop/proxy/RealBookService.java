0ab97979a4c06e6ff21519ad44e3ada0cc80caa8
JuHyun419 <zzang9ha@naver.com>
2021-09-23 20:18:55 +0900

    refactor: 코드 포맷팅

--- spring-example/spring-aop/src/main/java/com/juhyun/springaop/proxy/RealBookService.java
+++ spring-example/spring-aop/src/main/java/com/juhyun/springaop/proxy/RealBookService.java
@@ -7,9 +7,9 @@ import org.springframework.stereotype.Service;
 public class RealBookService implements BookService {
 
     /*
-        rent(): 책을 빌리는(핵심 기능) 메소드
-        책을 빌리기 전에 로깅(부가 기능)을 해야 한다고 가정
-        */
+    rent(): 책을 빌리는(핵심 기능) 메소드
+    책을 빌리기 전에 로깅(부가 기능)을 해야 한다고 가정
+    */
     public void rent(Book book) {
         System.out.println("rent: " + book.getTitle());
     }
