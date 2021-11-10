e1f6c25f589a85f513f0fe6d045c1b982bc9a5bb
JuHyun419 <zzang9ha@naver.com>
2021-11-10 21:23:06 +0900

    스프링 핵심 원리 고급편 initial commit

---
+++ books&lectures/(인프런)스프링 핵심 원리 고급편/src/main/java/hello/springadvanced/SpringAdvancedApplication.java
@@ -0,0 +1,13 @@
+package hello.springadvanced;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedApplication.class, args);
+    }
+
+}
---
+++ books&lectures/(인프런)스프링 핵심 원리 고급편/src/test/java/hello/springadvanced/SpringAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springadvanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
