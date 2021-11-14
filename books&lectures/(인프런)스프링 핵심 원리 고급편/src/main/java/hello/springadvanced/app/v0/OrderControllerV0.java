99e5b9f09b591f4644679129815ecadeafcad81a
JuHyun419 <zzang9ha@naver.com>
2021-11-14 17:33:58 +0900

    add v0

---
+++ books&lectures/(인프런)스프링 핵심 원리 고급편/src/main/java/hello/springadvanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,20 @@
+package hello.springadvanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+
+        return "ok";
+    }
+
+}
