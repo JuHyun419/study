72dfc560b17dfca8b6b2a102f68d643d18de8242
JuHyun419 <zzang9ha@naver.com>
2021-11-18 21:18:37 +0900

    initial commit

---
+++ spring-example/spring-fcm-example/src/main/java/com/example/springfcmexample/SpringFcmExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfcmexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFcmExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFcmExampleApplication.class, args);
+    }
+
+}
