319eefe1b9340791b3ce6990e0494646c0f7c071
JuHyun419 <zzang9ha@naver.com>
2021-11-25 11:00:00 +0900

    위클리챌린지 파일명 수정

---
+++ Algorithm/Programmers/src/main/java/weeklychallenge/직업군_추천하기_4주차.java
@@ -0,0 +1,56 @@
+package weeklychallenge;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class 직업군_추천하기_4주차 {
+
+    static class Jobs implements Comparable<Jobs> {
+        String jobGroup;
+        int score;
+
+        public Jobs(String jobGroup, int score) {
+            this.jobGroup = jobGroup;
+            this.score = score;
+        }
+
+        @Override
+        public int compareTo(Jobs o) {
+            if (this.score == o.score) {
+                return this.jobGroup.compareTo(o.jobGroup);
+            }
+            return Integer.compare(o.score, this.score);
+        }
+    }
+
+    public String solution(String[] table, String[] languages, int[] preference) {
+        Map<String, Integer> scores = new HashMap<>();
+        List<Jobs> list = new ArrayList<>();
+
+        for (int i = 0; i < languages.length; i++) {
+            scores.put(languages[i], preference[i]);
+        }
+
+        for (String s : table) {
+            String[] tables = s.split(" ");
+            int allScore = 0;
+            int score = 5;
+
+            /* 직업군별 점수 총합 구하기 */
+            for (int j = 1; j < tables.length; j++) {
+                if (scores.containsKey(tables[j])) {
+                    allScore += score * scores.get(tables[j]);
+                }
+                score--;
+            }
+
+            list.add(new Jobs(tables[0], allScore));
+        }
+
+        Collections.sort(list);
+        return list.get(0).jobGroup;
+    }
+}
