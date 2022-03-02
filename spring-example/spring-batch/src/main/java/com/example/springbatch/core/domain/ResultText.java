9704f11218d564c76edd67eef3c5f8feadd2ce9b
JuHyun419 <zzang9ha@naver.com>
2022-03-02 21:04:14 +0900

    feat: Entity, Repository

---
+++ spring-example/spring-batch/src/main/java/com/example/springbatch/core/domain/ResultText.java
@@ -0,0 +1,29 @@
+package com.example.springbatch.core.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@DynamicUpdate // 일부 컬럼의 값만 변경되었을 때, 변경된 값들에 대해서만 변경 쿼리가 실행
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "result_text")
+public class ResultText {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String text;
+}
---
+++ spring-example/spring-batch/src/main/java/com/example/springbatch/core/repository/ResultTextRepository.java
@@ -0,0 +1,7 @@
+package com.example.springbatch.core.repository;
+
+import com.example.springbatch.core.domain.ResultText;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResultTextRepository extends JpaRepository<ResultText, Integer> {
+}
