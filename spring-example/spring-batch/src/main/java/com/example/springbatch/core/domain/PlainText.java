18338ac40d635b60344b40565ce43d71ec974e3b
JuHyun419 <zzang9ha@naver.com>
2022-03-02 21:02:26 +0900

    feat: add @NoArgs, @AllArgs

--- spring-example/spring-batch/src/main/java/com/example/springbatch/core/domain/PlainText.java
+++ spring-example/spring-batch/src/main/java/com/example/springbatch/core/domain/PlainText.java
@@ -1,6 +1,8 @@
 package com.example.springbatch.core.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Column;
@@ -13,6 +15,8 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @DynamicUpdate // 일부 컬럼의 값만 변경되었을 때, 변경된 값들에 대해서만 변경 쿼리가 실행
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "plain_text")
 public class PlainText {
 
