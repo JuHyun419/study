41cc775c382285d3936ec416b0bafa9613aa18a3
JuHyun419 <zzang9ha@naver.com>
2022-05-09 23:10:46 +0900

    [ThreadLocal] 동시성 이슈 예제코드 추가

---
+++ books&lectures/(인프런)스프링 핵심 원리 고급편/src/test/java/hello/springadvanced/trace/threadlocal/code/FieldService.java
@@ -0,0 +1,27 @@
+package hello.springadvanced.trace.threadlocal.code;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class FieldService {
+
+    private String nameStore;
+
+    public String logic(String name) {
+        log.info("저장 name = {} -> nameStore = {}", name, nameStore);
+        nameStore = name;
+        sleep(1000);
+        log.info("조회 nameStore = {}", nameStore);
+
+        return nameStore;
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
